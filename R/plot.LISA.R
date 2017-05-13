4d2b8c5936d5c7b42be94bcf71ef512d46991f65
frajaroco <frajaroco@gmail.com>
2017-05-13 18:22:48 +0200

    Comment 13-05-2017-4

--- R/plot.LISA.R
+++ R/plot.LISA.R
@@ -1,4 +1,4 @@
-plot.LISA <- function(pl, main = "Product density LISA functions", xlab = "distances", ylab = "LISA", ...){
+plot.LISA <- function(pl, ...){
   
   verifyclass(pl$lisa, "matrix")
   verifyclass(pl$ds, "numeric" )
@@ -6,7 +6,7 @@ plot.LISA <- function(pl, main = "Product density LISA functions", xlab = "dista
   verifyclass(pl$rhotheo, "numeric")
   
   par(mfrow = c(1, 1))
-  plot(pl$ds,pl$lisa[,1], col = "#6B6B6B", type = "l",xlab = xlab, ylab = ylab, main = main, ylim=(c(0,max(pl$lisa))),...)
+  plot(pl$ds,pl$lisa[,1], col = "#6B6B6B", type = "l",xlab = "distances", ylab = "LISA", main = "Pair correlation LISA functions", ylim=(c(0,max(pl$lisa))),...)
   for (i in 2:dim(pl$lisa)[2]) {lines(pl$ds,pl$lisa[,i], col = "#6B6B6B")}
   lines(pl$ds, rep(pl$rhotheo, length(pl$ds)), col = "red")
 }

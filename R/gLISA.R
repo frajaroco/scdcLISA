f8997c8564bc0334081c4f54edb205780158b1c3
jagm03 <jmonsalv@uji.es>
2019-10-18 12:34:37 +0200

    Changes in edge correction. Proper weights and faster execution

--- R/gLISA.R
+++ R/gLISA.R
@@ -22,9 +22,7 @@ gLISA <- function(xy,ds,ks="epanechnikov",hs,lambda,correction="isotropic"){
   ker2 <- rep(0,3)
   ker2[ik] <- 1
   
-  if (missing(hs)){
-    hs <- bw.pcf(X=xy,kernel=ks)[1]
-  }
+  if (missing(hs)) hs <- bw.pcf(X=xy,kernel=ks)[1]
   
   bsw <- xy$window
   
@@ -34,50 +32,39 @@ gLISA <- function(xy,ds,ks="epanechnikov",hs,lambda,correction="isotropic"){
     ds <- seq(hs,maxd,len=301)[-1]
     ds <- sort(ds)
   }
-  if(ds[1]==0){
-    ds <- ds[-1]
-  }
   
-  kernel <- c(ks=ks,hs=hs)
+  if(ds[1]==0) ds <- ds[-1]
+  
+  kernel <- c(ks = ks, hs = hs)
   
   npt <- xy$n
   il <- seq(1,npt)
   nds <- length(ds)
   area <- area(bsw)
   githeo <- 1+(1/(npt-1))
- 	
- if(missing(lambda)){
-      lambda <- rep((npt-1)/area, npt)
-    }
+  
+  if(missing(lambda)) lambda <- rep((npt - 1) / area, npt)
   if (length(lambda) == 1) lambda <- rep(lambda, npt)
-
-
-  wrs <- array(0,dim=c(npt,npt))
-  d <- pairdist(xy)
   
-  if(correction=="isotropic"){
-    wisot <- edge.Ripley(xy,d)
-  }
+  if(correction=="isotropic") wrs <- edge.Ripley(xy, pairdist(xy))
   
-  lisa.g <- sapply(il, function(il) i.glisa(il,d,npt,ds,nds,ker2,hs,lambda,wrs,correc2,area),simplify="array")
+  lisa.g <- sapply(il, function(il) i.glisa(il,d,npt,ds,nds,ker2,hs,lambda,wrs,correc2,area), simplify = "array")
   
   invisible(return(list(lisa.g=lisa.g,ds=ds,kernel=kernel,lambda=lambda,githeo=githeo)))
 }
 
 i.glisa <- function(il,d,npt,ds,nds,ker2,hs,lambda,wrs,correc2,area){
-  
-	glisa <- rep(0,nds)
+  glisa <- rep(0, nds)
+	storage.mode(glisa) <- "double"
 	
-  storage.mode(glisa) <- "double"
-
- glisai <- .Fortran("coreglisa",il=as.integer(il),d=as.double(d),
-                    npt=as.integer(npt),ds=as.double(ds),
-                    nds=as.integer(nds),ker2=as.integer(ker2),
-                    hs=as.double(hs),lambda=as.double(lambda),
-                    wrs=as.double(wrs),correc2=as.integer(correc2),
-                   (glisa),PACKAGE="scdcLISA")
+	glisai <- .Fortran("coreglisa",il = as.integer(il), d = as.double(d),
+	                   npt = as.integer(npt), ds = as.double(ds),
+	                   nds = as.integer(nds), ker2 = as.integer(ker2),
+	                   hs = as.double(hs), lambda = as.double(lambda),
+	                   wrs = as.double(wrs), correc2 = as.integer(correc2),
+	                   (glisa), PACKAGE = "scdcLISA")
  
  glisas <- ((npt-1)*glisai[[11]])/(2*pi*area)
- 
- return(glisas=glisas)
+
+ return(glisas = glisas)
 }
